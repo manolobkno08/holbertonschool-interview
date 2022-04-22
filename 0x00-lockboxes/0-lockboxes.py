471b8f6303f6fa5efcaa3ac8c9680ae2e74e1581
manolobkno08 <manologm10@hotmail.com>
2022-04-21 22:53:46 -0500

    ADD: Unlock Boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+"""
+    You have n number of locked boxes in front of you. Each
+    box is numbered sequentially from 0 to n - 1 and each box
+    may contain keys to the other boxes.
+"""
+
+
 def canUnlockAll(boxes):
-    """
-    Unlock boxes and return
-    True or False
-    """
+    """Unlock boxes and return True or False"""
     unlockBoxes = [0]
 
     for key in unlockBoxes:
