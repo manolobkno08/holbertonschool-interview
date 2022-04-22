ada9b2d4edd6f14021e6cff57fce3a2e436d6087
manolobkno08 <manologm10@hotmail.com>
2022-04-21 22:46:13 -0500

    ADD: Unlock Boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Unlock boxes and return True or False"""
+    unlockBoxes = [0]
+
+    for key in unlockBoxes:
+        for item in boxes[key]:
+            if item not in unlockBoxes and item < len(boxes):
+                unlockBoxes.append(item)
+    if len(unlockBoxes) == len(boxes):
+        return True
+    else:
+        return False
